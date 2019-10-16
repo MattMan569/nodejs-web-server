74f7b519bc4ac71447d82dc6f9a6c739fb5b9bf6
MattMan569 <matthewpolsom@hotmail.com>
2019-10-16 17:35:35 -0600

    added dynamic pages via hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, './../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Matthew Polsom',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Matthew Polsom',
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpMessage: 'There is no help to be found.',
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
