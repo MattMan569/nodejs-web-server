7e9965f04c5658d6793e7a0016a91284ed3faf82
MattMan569 <matthewpolsom@hotmail.com>
2019-10-16 17:43:45 -0600

    customized view path

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,19 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, './../public');
 
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '/../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+// Setup routes
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -34,6 +43,7 @@ app.get('/weather', (req, res) => {
     });
 });
 
+// Start the server
 app.listen(3000, () => {
     console.log('Server is up on port 3000!');
 });
