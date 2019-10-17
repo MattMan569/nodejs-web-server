5415bcb4acb21a6190d9f7f6e573f2528c03f67e
MattMan569 <matthewpolsom@hotmail.com>
2019-10-17 16:04:41 -0600

    basic handling of weather api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,16 @@
 console.log('Client side js file');
+
+(async () => {
+    const response = await fetch('http://localhost:3000/weather?address=regina');
+    const data = await response.json();
+
+    if (data.error) {
+        return console.log(data.error);
+    }
+
+    const {forecast, location, address} = data;
+
+    console.log(forecast);
+    console.log(location);
+    console.log(address);
+})();
