191f766a2a846e0a8d4c9b1fd4ffb2351bb5397c
MattMan569 <matthewpolsom@hotmail.com>
2019-10-17 16:53:23 -0600

    setup weather UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,16 +1,27 @@
-console.log('Client side js file');
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
 
-(async () => {
-    const response = await fetch('http://localhost:3000/weather?address=regina');
-    const data = await response.json();
+    const search = document.querySelector('input');
+    const message1 = document.querySelector('#message-1');
+    const message2 = document.querySelector('#message-2');
 
-    if (data.error) {
-        return console.log(data.error);
-    }
+    message1.textContent = 'Loading...';
+    message2.textContent = '';
 
-    const {forecast, location, address} = data;
+    (async () => {
+        const response = await fetch(`http://localhost:3000/weather?address=${search.value}`);
+        const data = await response.json();
 
-    console.log(forecast);
-    console.log(location);
-    console.log(address);
-})();
+        if (data.error) {
+            message1.textContent = data.error;
+            return;
+        }
+
+        search.value = '';
+
+        const {forecast, location} = data;
+
+        message1.textContent = location;
+        message2.textContent = forecast;
+    })();
+});
